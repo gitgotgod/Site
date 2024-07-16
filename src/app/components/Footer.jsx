4fffc6db2b88fc3ca7361d213117faf4666222bc
Jason Chung <125996156+gitgotgod@users.noreply.github.com>
2024-07-17 00:30:20 +0900

    Add files via upload
    updated files

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import config from "../index.json";
+import {
+  AiOutlineTwitter,
+  AiOutlineGithub,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
+
+const Footer = () => {
+  const footer = config.footer;
+  const getYear = () => {
+    return new Date().getFullYear();
+  };
+
+  return (
+    <div className="footer px-8 py-10 flex justify-center align-center flex-col bg-white">
+      <h1 className="text-center text-2xl text-slate-500">Follow Me</h1>
+      <div className="mx-auto text-3xl  mt-5 mb-8 space-x-10 flex">
+        <a rel="noreferrer" href={footer.twitter} target="_blank">
+          <AiOutlineTwitter
+            size={48}
+            className="fill-slate-500 hover:fill-blue-500"
+          />
+        </a>
+        <a rel="noreferrer" href={footer.linkedin} target="_blank">
+          <AiOutlineLinkedin
+            size={48}
+            className="fill-slate-500 hover:fill-blue-500"
+          />
+        </a>
+        <a rel="noreferrer" href={footer.github} target="_blank">
+          <AiOutlineGithub
+            size={48}
+            className="fill-slate-500 hover:fill-blue-500"
+          />
+        </a>
+      </div>
+
+      <span className="text-sm text-center text-blue-400">
+        © {getYear()}
+        <a href="https://samcha.vercel.app/"> Jason Chung Portfolio</a>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,84 @@
+"use client";
+import React from "react";
+import config from "../index.json";
+import { TypeAnimation } from "react-type-animation";
+import {
+  AiOutlineTwitter,
+  AiOutlineGithub,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
+import { Link } from "react-scroll";
+const Hero = () => {
+  const hero = config.hero;
+  const footer = config.footer;
+  const about = config.about;
+
+  return (
+    <section className="h-screen bg-white  ">
+      <div className="px-8 md:px-32 flex flex-col justify-center  ">
+        <h1 className="text-3xl flex  mt-32 md:text-5xl md:mt-64 font-bold tracking-wide">
+          Hi, my name is
+          <span className=" ml-3 whitespace-nowrap bg-gradient-to-r from-blue-800 to-blue-400 bg-clip-text text-transparent">
+            {hero.name}
+          </span>
+        </h1>
+        <div className="flex justify-start">
+          <h1 className=" text-3xl  md:text-6xl mt-4 font-bold tracking-wide">
+            <p>I&apos;m a</p>
+            <div className="pt-4 whitespace-nowrap bg-gradient-to-r from-blue-800 to-blue-400 bg-clip-text text-transparent">
+              <TypeAnimation
+                sequence={[
+                  "Full-stack Developer.",
+                  2000, // Waits 1s
+                  "React-Developer.",
+                  2500, // Waits 2s
+                  "Digital Media Specialist.",
+                  2500,
+                ]}
+                wrapper="span"
+                cursor={true}
+                repeat={Infinity}
+              />
+            </div>
+          </h1>
+        </div>
+        <Link
+          to="About"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+          className="text-2xl font-bold p-0.5 mt-6 w-44 bg-gradient-to-r from-blue-400  to-indigo-500  "
+        >
+          <div className="bg-white ">
+            <span className="block text-center py-0.5 px-2  bg-white font-bold bg-gradient-to-r from-blue-400  to-indigo-800 bg-clip-text text-transparent  hover:text-blue-300 delay-100">
+              Learn More
+            </span>
+          </div>
+        </Link>
+        <div className="flex gap-4 mt-3">
+          <a rel="noreferrer" href={footer.twitter} target="_blank">
+            <AiOutlineTwitter
+              size={40}
+              className="fill-slate-600 hover:fill-blue-600"
+            />
+          </a>
+          <a rel="noreferrer" href={footer.github} target="_blank">
+            <AiOutlineGithub
+              size={40}
+              className="fill-slate-600 hover:fill-blue-600"
+            />
+          </a>
+          <a rel="noreferrer" href={footer.linkedin} target="_blank">
+            <AiOutlineLinkedin
+              size={40}
+              className="fill-slate-600 hover:fill-blue-600"
+            />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
